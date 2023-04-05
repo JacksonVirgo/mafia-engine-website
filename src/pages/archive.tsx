7aedda7d71a503855bb09120f4b8ea50ddde5896
Jack Nash <jacknash42@gmail.com>
2023-04-05 10:34:55 +1000

    add channel option selector

--- src/pages/archive.tsx
+++ src/pages/archive.tsx
@@ -7,75 +7,97 @@ import { Channel, DiscordAccount, Message } from "@prisma/client";
 import MessageComp from "~/components/Message";
 
 type MessageData = Message & {
-  author: DiscordAccount;
+    author: DiscordAccount;
 };
 
 // Bubble Sort. Earliest = first
 function reorderMessages(msg: MessageData[]) {
-  const len = msg.length;
-  const array = msg;
-  for (let i = 0; i < len; i++) {
-    for (let j = 0; j < len; j++) {
-      const jVal = array[j];
-      const jValAdd = array[j + 1];
+    const len = msg.length;
+    const array = msg;
+    for (let i = 0; i < len; i++) {
+        for (let j = 0; j < len; j++) {
+            const jVal = array[j];
+            const jValAdd = array[j + 1];
 
-      if (jVal && jValAdd) {
-        if (jVal.createdAt.getTime() < jValAdd.createdAt.getTime()) {
-          [array[j], array[j + 1]] = [jValAdd, jVal];
+            if (jVal && jValAdd) {
+                if (jVal.createdAt.getTime() < jValAdd.createdAt.getTime()) {
+                    [array[j], array[j + 1]] = [jValAdd, jVal];
+                }
+            }
         }
-      }
     }
-  }
 
-  return msg;
+    return msg;
 }
 
 const ChannelArchive: NextPage = () => {
-  const router = useRouter();
-  const { id } = router.query;
-  const pageData = api.archive.getChannel.useQuery({
-    // channelId: id ? (id as string) : "",
-    channelId: "1082940469203435551",
-    take: 500,
-    skip: 0,
-  });
-  const [channel, setChannel] = useState<Channel>();
-  const [messages, setMessages] = useState<MessageData[]>([]);
+    const router = useRouter();
+    const { id } = router.query;
+    const pageData = api.archive.getChannel.useQuery({
+        // channelId: id ? (id as string) : "",
+        channelId: "1082940469203435551",
+        take: 25,
+        skip: 0,
+    });
 
-  useEffect(() => {
-    if (pageData.isFetched) {
-      const channel = pageData.data?.channel;
-      if (channel) {
-        setChannel(channel);
-        const ordered = reorderMessages(channel.messages);
-        setMessages(ordered);
-      }
-    }
-  }, [pageData]);
-  return (
-    <>
-      <Head>
-        <title>Discord Mafia Archives</title>
-        <meta
-          name="description"
-          content="Archive of a specific channel (this is the same with all pages for now, will change)"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    const { data: channels, isLoading: channelsLoading } =
+        api.archive.getAllChannels.useQuery({
+            take: 25,
+            skip: 0,
+        });
 
-      <main className="bg-discord-dark p-8 text-white">
-        <div className="pl-4">
-          <div>
-            CHANNEL NAME: {channel?.name ? `#${channel.name}` : "LOADING"}
-          </div>
-          <div>{id}</div>
-        </div>
-        {messages.map((v) => {
-          return <MessageComp msg={v} key={v.messageId} />;
-        })}
-      </main>
-    </>
-  );
+    const [channel, setChannel] = useState<Channel>();
+    const [messages, setMessages] = useState<MessageData[]>([]);
+
+    useEffect(() => {
+        if (pageData.isFetched) {
+            const channel = pageData.data?.channel;
+            if (channel) {
+                setChannel(channel);
+                const ordered = reorderMessages(channel.messages);
+                setMessages(ordered);
+            }
+        }
+    }, [pageData]);
+    return (
+        <>
+            <Head>
+                <title>Discord Mafia Archives</title>
+                <meta
+                    name="description"
+                    content="Archive of a specific channel (this is the same with all pages for now, will change)"
+                />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <main className="flex h-screen flex-row bg-discord-dark bg-polygon p-8 text-white">
+                <div className="w-96 bg-discord-darker">
+                    {channelsLoading
+                        ? "Loading..."
+                        : channels?.map((v) => {
+                              return (
+                                  <div
+                                      className="py-2 pl-2 text-base hover:cursor-pointer hover:bg-gray-600"
+                                      key={v.channelId}
+                                  >
+                                      #{v.name}
+                                  </div>
+                              );
+                          })}
+                </div>
+                <div className="flex grow flex-col bg-discord-dark pl-4">
+                    <div className="border-b-2 border-b-black py-4">
+                        #{channel?.name}
+                    </div>
+                    <div className="overflow-x-auto overflow-y-scroll ">
+                        {messages.map((v) => {
+                            return <MessageComp msg={v} key={v.messageId} />;
+                        })}
+                    </div>
+                </div>
+            </main>
+        </>
+    );
 };
 
 export default ChannelArchive;
--- src/server/api/routers/archive.ts
+++ src/server/api/routers/archive.ts
@@ -1,94 +1,126 @@
 import { z } from "zod";
 
 import {
-  createTRPCRouter,
-  publicProcedure,
-  //   protectedProcedure,
+    createTRPCRouter,
+    publicProcedure,
+    privateProcedure,
 } from "~/server/api/trpc";
 
 function extractMentionedUserId(mention: string): string | undefined {
-  const regex = /<@(\S+)>/;
-  const match = mention.match(regex);
-  return match ? match[1] : undefined;
+    const regex = /<@(\S+)>/;
+    const match = mention.match(regex);
+    return match ? match[1] : undefined;
 }
 
 export const archiveRouter = createTRPCRouter({
-  getChannel: publicProcedure
-    .input(
-      z.object({
-        channelId: z.string().nullish(),
-        take: z.number().nullish(),
-        skip: z.number().nullish(),
-      })
-    )
-    .query(async ({ ctx, input }) => {
-      const { channelId, take, skip } = input;
-      if (!channelId)
-        return { channel: null, error: "Does not have ChannelID" };
+    getChannel: publicProcedure
+        .input(
+            z.object({
+                channelId: z.string().nullish(),
+                take: z.number().nullish(),
+                skip: z.number().nullish(),
+            })
+        )
+        .query(async ({ ctx, input }) => {
+            const { channelId, take, skip } = input;
+            if (!channelId)
+                return { channel: null, error: "Does not have ChannelID" };
 
-      const takeAmount = take || 25;
-      const skipAmount = skip || 0;
+            const takeAmount = take || 25;
+            const skipAmount = skip || 0;
 
-      try {
-        const data = await ctx.prisma.channel.findUnique({
-          where: {
-            channelId: channelId,
-          },
-          include: {
-            messages: {
-              take: takeAmount,
-              skip: skipAmount,
-              include: {
-                author: true,
-              },
-            },
-          },
-        });
+            try {
+                const data = await ctx.prisma.channel.findUnique({
+                    where: {
+                        channelId: channelId,
+                    },
+                    include: {
+                        messages: {
+                            take: takeAmount,
+                            skip: skipAmount,
+                            include: {
+                                author: true,
+                            },
+                        },
+                    },
+                });
 
-        if (!data)
-          return {
-            channel: null,
-            error: "Channel does not exist",
-          };
+                if (!data)
+                    return {
+                        channel: null,
+                        error: "Channel does not exist",
+                    };
 
-        return {
-          channel: data,
-          error: null,
-        };
-      } catch (err) {
-        console.log(err);
-        return { channel: null, error: "Unexpected error" };
-      }
-    }),
+                return {
+                    channel: data,
+                    error: null,
+                };
+            } catch (err) {
+                console.log(err);
+                return { channel: null, error: "Unexpected error" };
+            }
+        }),
 
-  getUser: publicProcedure
-    .input(z.object({ userId: z.string() }))
-    .query(async ({ ctx, input }) => {
-      const extract = extractMentionedUserId(input.userId);
+    getUser: publicProcedure
+        .input(z.object({ userId: z.string() }))
+        .query(async ({ ctx, input }) => {
+            const extract = extractMentionedUserId(input.userId);
 
-      try {
-        if (!extract) throw Error();
-        const user = await ctx.prisma.discordAccount.findFirst({
-          where: {
-            discordId: extract,
-          },
-        });
+            try {
+                if (!extract) throw Error();
+                const user = await ctx.prisma.discordAccount.findFirst({
+                    where: {
+                        discordId: extract,
+                    },
+                });
 
-        if (!user)
-          return {
-            user: null,
-            status: 404,
-            input: input.userId,
-            formatted: extract,
-          };
-        return { user, status: 200, input: input.userId, formatted: extract };
-      } catch (err) {
-        return {
-          user: null,
-          status: 500,
-          input: input.userId,
-          formatted: extract,
-        };
-      }
+                if (!user)
+                    return {
+                        user: null,
+                        status: 404,
+                        input: input.userId,
+                        formatted: extract,
+                    };
+                return {
+                    user,
+                    status: 200,
+                    input: input.userId,
+                    formatted: extract,
+                };
+            } catch (err) {
+                return {
+                    user: null,
+                    status: 500,
+                    input: input.userId,
+                    formatted: extract,
+                };
+            }
+        }),
+
+    getAllChannels: publicProcedure
+        .input(
+            z.object({
+                take: z.number().nullish(),
+                skip: z.number().nullish(),
+            })
+        )
+        .query(async ({ ctx, input }) => {
+            const takeAmount = input.take || 25;
+            const skipAmount = input.skip || 0;
+            const data = await ctx.prisma.channel.findMany({
+                take: takeAmount,
+                skip: skipAmount,
+                select: {
+                    name: true,
+                    channelId: true,
+                },
+            });
+
+            return data;
+        }),
+    getCache: privateProcedure.query(async ({ ctx }) => {
+        const authorId = ctx.currentUser.id;
+
+        return {};
     }),
 });
